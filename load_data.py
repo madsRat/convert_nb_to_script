9ba79eb64548567ab6f89580f590c1927d0b6b61
madsRat <torrecam@umich.edu>
2024-03-16 23:25:12 -0400

    added main.py file, plot_data.py, and fixed load_data.py to include args

--- load_data.py
+++ load_data.py
@@ -7,6 +7,6 @@ def load_csv_data(vehicle_file_path, person_file_path):
     :param vehicle_file_path, person_file_path: the csv file path for vehicle data and person data
     :return Two Dataframes, one vehicle df and one person df
     """
-    df_vehicle = pd.read_csv('data/crss_2021_csv/vehicle.csv')
-    df_person = pd.read_csv('data/crss_2021_csv/person.csv')
+    df_vehicle = pd.read_csv(vehicle_file_path) # 'data/crss_2021_csv/vehicle.csv'
+    df_person = pd.read_csv(person_file_path) # 'data/crss_2021_csv/person.csv'
     return df_vehicle, df_person
